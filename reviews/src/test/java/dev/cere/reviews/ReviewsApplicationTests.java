ada5ab959772ef554424e2d18a96ef786fff1cff
Stanislav Zeman <zeman@standa.dev>
2024-03-22 18:08:08 +0100

    chore(reviews): initialize submodule

---
+++ reviews/src/test/java/dev/cere/reviews/ReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cere.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
