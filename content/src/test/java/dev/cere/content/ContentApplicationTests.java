c26d3d8864958094c3bd874d48925ca5f07cb9d4
Stanislav Zeman <zeman@standa.dev>
2024-03-22 18:07:10 +0100

    chore(content): initialize submodule

---
+++ content/src/test/java/dev/cere/content/ContentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cere.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
