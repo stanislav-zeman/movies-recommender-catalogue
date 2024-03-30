95b873d56ce6400e2a6b13a415714f03ee8652e7
Stanislav Zeman <zeman@standa.dev>
2024-03-30 18:30:42 +0100

    feat(gateway): implement basic gateway

--- gateway/src/main/java/dev/cere/gateway/GatewayApplication.java
+++ gateway/src/main/java/dev/cere/gateway/GatewayApplication.java
@@ -1,12 +1,43 @@
 package dev.cere.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+    @Value("${services.content.uri}")
+    private String contentServiceUri;
+
+    @Value("${services.users.uri}")
+    private String usersServiceUri;
+
+    @Value("${services.recommendations.uri}")
+    private String recommendationsServiceUri;
+
+    @Value("${services.reviews.uri}")
+    private String reviewsServiceUri;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        System.out.println("recommendationsServiceUri: " + recommendationsServiceUri);
+        return builder.routes()
+                .route(r -> r.path("/content/*").or().path("/content").uri(contentServiceUri))
+                .route(r -> r.path("/users/*").or().path("/users").uri(usersServiceUri))
+                .route(
+                        r ->
+                                r.path("/recommendations/*")
+                                        .or()
+                                        .path("/recommendations")
+                                        .uri(recommendationsServiceUri))
+                .route(r -> r.path("/reviews/*").or().path("/reviews").uri(reviewsServiceUri))
+                .build();
     }
 }
--- gateway/src/test/java/dev/cere/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/dev/cere/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package dev.cere.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatewayApplication.class)
 class GatewayApplicationTests {
 
     @Test
